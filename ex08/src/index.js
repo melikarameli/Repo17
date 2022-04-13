b2f70b1ad091e55d6780ce0404272293965313b3
melikarameli <melikahatunic2001@gmail.com>
2022-04-13 16:49:38 +0200

    Added ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+   // users nested array with four objects starts here
+
+   // users nesstes array with four objects ends here
+   // getUsers function - list of users starts here
+
+   // getUsers function - list of users ends here
+   // findUser(lastName, gender) function starts here
+
+   // findUser(lastName, gender) function ends here
+// Only change code above this line
+getUsers();
+findUser(); // Change this line
+
+module.exports = findUser;
